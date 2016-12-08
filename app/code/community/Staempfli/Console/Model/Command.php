7e80c50946a9583d1170b5766b792120ceea1988
alojua <juan.alonso@staempfli.com>
2016-12-08 21:13:01 +0100

    remove php exe^tension from command access file and not used classes

--- app/code/community/Staempfli/Console/Model/Command.php
+++ app/code/community/Staempfli/Console/Model/Command.php
@@ -7,8 +7,6 @@
  * @author    juan.alonso@staempfli.com
  */
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class Staempfli_Console_Model_Command extends Command
 {
